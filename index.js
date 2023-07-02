a68935fcbadf644c21ab3957430a88698aa0aedd
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-07-02 14:15:32 +0600

    sometimes data not found issue are solved

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-
+// Set Middleware
 app.use(cors());
-app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5a8lj4m.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,45 +17,44 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const projectDataCollection = client.db('portfolioDb').collection('portfolioData');
 
-
-    app.get('/project', async (req, res ) => {
-      const curson = projectDataCollection.find();
-      const result = await curson.toArray();
+    app.get('/project', async (req, res) => {
+      const cursor = projectDataCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
-  })
+    });
 
-  app.get('/project/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await projectDataCollection.findOne(query);
-    res.send(result);
-  })
+    app.get('/project/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await projectDataCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('the portfolio server are running');
-})
+  res.send('The portfolio server is running');
+});
 
 app.listen(port, () => {
-    console.log(`the server runing on this ${port}`);
-})
+  console.log(`The server is running on port ${port}`);
+});
